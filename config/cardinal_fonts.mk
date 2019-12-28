bc1d7fddd6e34b2b4f6db50cbd41b5f7b3dead48
kubersharma001 <kubersharma001@gmail.com>
2019-12-28 13:44:58 +0100

    fonts: Drop prebuilt fonts causing overriding commands
     * Get them from external/google-fonts
       ref: android_build/commit/288ec50c08295caef52135a810bb3428d481746b
    
    	overriding commands for target `/../../../../../../../system/product/fonts/ArbutusSlab-Regular.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Lato-BoldItalic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Lato-Bold.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Lato-Italic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Lato-MediumItalic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Lato-Medium.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Lato-Regular.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Rubik-BoldItalic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Rubik-Bold.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Rubik-Italic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Rubik-MediumItalic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Rubik-Medium.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/Rubik-Regular.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/ZillaSlab-MediumItalic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/ZillaSlab-Medium.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/ZillaSlab-SemiBoldItalic.ttf'
    	overriding commands for target `/../../../../../../../system/product/fonts/ZillaSlab-SemiBold.ttf'
    
    Change-Id: I19c173072983469cb85152cdd91a80399163dfe6
    Signed-off-by: Kizziama <kizziama@proton.me>

--- config/cardinal_fonts.mk
+++ config/cardinal_fonts.mk
@@ -4,36 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Google fonts
+$(call inherit-product-if-exists, external/google-fonts/arbutus-slab/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/arvo/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
+$(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
+
 PRODUCT_COPY_FILES += \
-    vendor/cardinal/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
-    vendor/cardinal/fonts/ArbutusSlab-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/ArbutusSlab-Regular.ttf \
     vendor/cardinal/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
     vendor/cardinal/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
     vendor/cardinal/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/cardinal/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
     vendor/cardinal/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/cardinal/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
-    vendor/cardinal/fonts/Lato-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Lato-BoldItalic.ttf \
-    vendor/cardinal/fonts/Lato-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Lato-Bold.ttf \
-    vendor/cardinal/fonts/Lato-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Lato-Italic.ttf \
-    vendor/cardinal/fonts/Lato-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Lato-MediumItalic.ttf \
-    vendor/cardinal/fonts/Lato-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Lato-Medium.ttf \
-    vendor/cardinal/fonts/Lato-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Lato-Regular.ttf \
-    vendor/cardinal/fonts/Rubik-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Rubik-BoldItalic.ttf \
-    vendor/cardinal/fonts/Rubik-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Rubik-Bold.ttf \
-    vendor/cardinal/fonts/Rubik-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Rubik-Italic.ttf \
-    vendor/cardinal/fonts/Rubik-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Rubik-MediumItalic.ttf \
-    vendor/cardinal/fonts/Rubik-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Rubik-Medium.ttf \
-    vendor/cardinal/fonts/Rubik-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Rubik-Regular.ttf \
-    vendor/cardinal/fonts/ZillaSlab-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/ZillaSlab-MediumItalic.ttf \
-    vendor/cardinal/fonts/ZillaSlab-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/ZillaSlab-Medium.ttf \
-    vendor/cardinal/fonts/ZillaSlab-SemiBoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/ZillaSlab-SemiBoldItalic.ttf \
-    vendor/cardinal/fonts/ZillaSlab-SemiBold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/ZillaSlab-SemiBold.ttf
+    vendor/cardinal/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
 
 PRODUCT_PACKAGES += \
+    FontGoogleSansLatoOverlay \
+    FontGoogleSansOverlay \
     FontArbutusSourceOverlay \
     FontArvoLatoOverlay \
-    FontRubikRubikOverlay \
-    FontGoogleSansLatoOverlay \
-    FontGoogleSansOverlay
+    FontRubikRubikOverlay
