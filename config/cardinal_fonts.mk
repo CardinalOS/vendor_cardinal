ffdb6e34de3cf0672922e8ffa5f3e99d69e62459
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-03-21 16:46:00 +0530

    fonts: Add OnePlus Slate font overlay package
    - fonts taken from op7 dump
    
    Change-Id: I09174f966f3854627b6523f8cf14468c0628d1c5

--- config/cardinal_fonts.mk
+++ config/cardinal_fonts.mk
@@ -10,16 +10,21 @@ $(call inherit-product-if-exists, external/google-fonts/lato/fonts.mk)
 $(call inherit-product-if-exists, external/google-fonts/rubik/fonts.mk)
 
 PRODUCT_COPY_FILES += \
+    vendor/cardinal/fonts/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
     vendor/cardinal/fonts/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
     vendor/cardinal/fonts/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
     vendor/cardinal/fonts/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
     vendor/cardinal/fonts/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
     vendor/cardinal/fonts/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/cardinal/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
+    vendor/cardinal/fonts/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf \
+    vendor/cardinal/fonts/SlateForOnePlus-Book.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/SlateForOnePlus-Book.ttf \
+    vendor/cardinal/fonts/SlateForOnePlus-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/SlateForOnePlus-Medium.ttf \
+    vendor/cardinal/fonts/SlateForOnePlus-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/SlateForOnePlus-Regular.ttf
 
 PRODUCT_PACKAGES += \
-    FontGoogleSansLatoOverlay \
-    FontGoogleSansOverlay \
     FontArbutusSourceOverlay \
     FontArvoLatoOverlay \
+    FontGoogleSansOverlay \
+    FontGoogleSansLatoOverlay \
+    FontSlateForOnePlusOverlay \
     FontRubikRubikOverlay
